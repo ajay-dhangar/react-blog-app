8f55f15fd04b8866d1a7dbff01e481dab5a3b398
Ajay-Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-07-26 09:12:55 +0530

    updated

--- src/pages/navbar/Navbar.jsx
+++ src/pages/navbar/Navbar.jsx
@@ -1,64 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import './Navbar.css'
 
 function Navbar() {
-  
+  const [isAsideOpen, setIsAsideOpen] = useState(false);
+
+  const toggleAside = () => {
+    setIsAsideOpen((prev) => !prev);
+  };
 
   return (
     <div>
-      <div className='header'>        
-          <span className="roted">The</span>
-          <span className='normal'>SIREN</span>
+      <div className='header'>
+        <span className="roted">The</span>
+        <span className='normal'>SIREN</span>
       </div>
-      <nav className="navbar" id="navbar">
-        <div className="container">
 
-          {/* <Link className="" to={'#'}>
-            React Blog App
-          </Link> */}
-
-          {/* <button onClick={handleNavToggler}>
-            <span className="navbar-toggler-icon"></span>
-          </button> */}
-
-          <div className='' id="navbarResponsive">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link" to={'/Home'}>
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={'/Bollywood'}>
-                  Bollywood
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={'/Technology'}>
-                  Technology
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={'/Hollywood'}>
-                  Hollywood
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={'/Fitness'}>
-                  Fitness
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={'/Food'}>
-                  Food
-                </Link>
-              </li>
-            </ul>
+      <div className={`aside ${isAsideOpen ? 'open' : ''}`}>
+        <div className="nav-toggler" onClick={toggleAside}>
+          <span></span>
+        </div>
+        
+        <nav className="navbar" id="navbar">
+          <div className="container">
+            <div className='' id="navbarResponsive">
+              <ul className="nav navbar-nav">
+                <li className="nav-item">
+                  <Link to="/Home" className="nav-link"><i class="fa fa-home"></i> Home </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className='nav-link' to="/Bollywood"><i className="fa fa-user"></i> Bollywood </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className='nav-link' to="/Technology"><i className="fa fa-list"></i> Technologys </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className='nav-link' to="/Hollywood"><i className="fa fa-briefcase"></i> Hollywood </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className='nav-link' to="/Fitness"><i className="fa fa-comments"></i> Fitness </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className='nav-link' to="/Food"><i className="fa fa-comments"></i> Food </Link>
+                </li>
+              </ul>
+            </div>            
           </div>
           <hr className='nav-bottom-line' />
-        </div>
-      </nav>
+        </nav>
+      </div>
     </div>
   );
 }
